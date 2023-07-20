e695dd47b8bd9f1d104900c0310f4022fb370a74
chaeha293@naver.com <chaeha@kyowon.co.kr>
2023-07-20 17:57:10 +0900

    섹션 4. 독립 실행형 스프링 애플리케이션-의존 오브젝트 DI 적용

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -22,24 +23,25 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.refresh();
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			HelloController helloController = new HelloController();
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
+			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					// 인증, 보안, 다국어, 공통 기능
 					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						HelloController helloController = applicationContext.getBean(HelloController.class);
 						String ret = helloController.hello(name);
 
-						resp.setStatus(HttpStatus.OK.value()); // Status Code
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Header
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE); // Header
 						resp.getWriter().println(ret); // Body
-					} else if (req.getRequestURI().equals("/user")) {
-
 					} else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
@@ -47,7 +49,6 @@ public class HellobootApplication {
 			}).addMapping("/*");
 		});
 		webServer.start();
-		// SpringApplication.run(HellobootApplication.class, args);
 	}
 
 }
