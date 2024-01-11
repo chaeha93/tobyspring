faf2341d5d3d9e94bbcbafd38f4db178337e86b0
chaeha@kyowon.co.kr-at-651800891045~ <chaeha@kyowon.co.kr>
2024-01-12 08:57:38 +0900

    섹션 5-1. 테스트 코드를 이용한 테스트

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,6 +1,5 @@
 package tobyspring.helloboot;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +10,9 @@ import java.util.Objects;
 public class HelloController {
 
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
